af5a580c351df74a2e4750be6906718c9da1011f
Airam <139919189+viniairam7@users.noreply.github.com>
2023-09-21 10:02:33 -0300

    Create Maioridade.py

---
+++ Maioridade.py
@@ -0,0 +1,12 @@
+from datetime import date
+atual = date.today().year
+totmaior = 0
+totmenor = 0
+for pess in range(1, 5):
+    nasc = int(input('Em que ano a {}° pessoa nasceu? :  '.format(pess)))
+    idade = atual - nasc
+    if idade >= 18:
+        totmaior += 1
+    else:
+        totmenor += 1
+print('Ao todo, {} são maiores e {} são menores.'.format(totmaior, totmenor))
